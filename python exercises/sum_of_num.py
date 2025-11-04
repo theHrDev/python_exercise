7598c042379be5f5cda1e23efac854e2e6309ef8
theHrDev <hamzatrukayat2822@gmail.com>
2025-11-04 10:15:37 +0100

    Added the function that added together the number in a loop

---
+++ python exercises/sum_of_num.py
@@ -0,0 +1,15 @@
+val = 0
+for num in range(11):
+    # print(num)
+    val += num
+print(val)
+
+
+# using while loop
+val = 0
+num = 0
+while num <10:
+    num += 1
+    # print(num)
+    val += num
+print(val)
