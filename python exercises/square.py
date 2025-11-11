850e0cb5b01960b77d79f9e580c3718202774011
theHrDev <hamzatrukayat2822@gmail.com>
2025-11-11 17:27:01 +0100

    Added the challenge

---
+++ python exercises/square.py
@@ -0,0 +1,9 @@
+number =  int(input("Input the number to print the integer lesser than it: "))
+# while number >= 0:
+#     number -=1
+#     print(number)
+
+
+for num in range(number):
+    squa = num * num
+    print(squa)
