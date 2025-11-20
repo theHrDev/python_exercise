fcbb964a242b04e33450a7d8c1398b985aebf000
theHrDev <hamzatrukayat2822@gmail.com>
2025-11-20 14:51:56 +0100

    Added the vowel counting program

--- python exercises/vowels.py
+++ python exercises/vowels.py
@@ -2,8 +2,12 @@
 vowels = ["a","e","i","o","u"]
 
 user_word =  input("Enter a word: ")
-for word in user_word:
-    if word is vowels:
-            print("Vowel")
-    else:
-            print("Consonant")
+vowel_count = 0
+for i in range(int(len(user_word))):
+        # print(i)
+    if user_word[i] == "a" or user_word[i] == "e" or user_word[i] == "i" or user_word[i] == "o" or user_word[i] == "u":
+            vowel_count += 1
+            print(f"{user_word[i]}")
+            print(f"The number of vowel in the word {user_word} is {vowel_count}")
+#     else:
+#             print("Consonant")
